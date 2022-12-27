249cbf07764dd0b0e87c1689464a21445c6b0a15
Emine Tunç <eminetunc35@hotmail.com>
2022-12-28 02:11:16 +0300

    Proje dosyası ekle.

---
+++ ogrenci.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Odev
+{
+    internal class Ogrenci
+    {
+        private int Ogrenci_No;
+        private string Isım;
+        private string Soyısım;
+        private string okulısmı;
+        private int vize1;
+        private int vize2;
+        private int final;
+       
+        public Ogrenci(int Ogrenci_No, string Isım, string Soyısım, string okulısmı, int vize1, int vize2, int final)
+        {
+            this.Ogrenci_No = Ogrenci_No; //“this” anahtar kelimesi, ilgili nesnenin referansını belirtmek için kullanılır.
+            this.Isım = Isım;
+            this.Soyısım = Soyısım;
+            this.okulısmı = okulısmı;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+        }
+        public void Ogrenci_Bilgilerini_Goster()
+        {
+            Console.WriteLine("Öğrenci numarası: {0}\nÖğrenci ad soyadı: {1} {2}", Ogrenci_No, Isım, Soyısım);
+        }
+
+        public void Ortalama_Bul()
+        {
+            int vizeort = (vize1 + vize2) / 2;
+            int ort = Convert.ToInt32(vizeort * 0.4 + final * 0.6);
+            Console.WriteLine("Öğrencinin ortalaması: " + ort);
+        }
+
+        public void Okulunu_Goster()
+        {
+            Console.WriteLine("Öğrencinin Okulu: "  + okulısmı);
+        }
+
+         public void cıkıs_yap()
+        {
+            Console.WriteLine("çıkış yapıldı " );
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace Odev
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci (456, "Emine", "Tunç", "karabük Üniversitesi", 25, 85, 70);
+
+            
+
+
+            while (true)
+            {
+                Console.WriteLine("\nBirini Seçiniz: \n1) Öğrenci bilgileri Göster\n2) Öğrencinin ortalamasını Göster \n3) Öğrencinin okulunu Göster\n4) Çıkış Yap \n\n\n\nTERCİHİNİZ:");
+                int Kullanıcı_Girisi = Convert.ToInt32(Console.ReadLine());
+               
+                Console.WriteLine("\n\t");
+
+                switch (Kullanıcı_Girisi)
+                {
+                    case 1:
+                        ogr.Ogrenci_Bilgilerini_Goster();
+                        break;
+                    case 2:
+                        ogr.Ortalama_Bul();
+                        break;
+                    case 3:
+                        ogr.Okulunu_Goster();
+                        break;
+                    case 4:
+                        ogr.cıkıs_yap();
+                        break;
+                    default:
+                        Console.WriteLine("hatalı tercih!");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Başka bir işlem yapmak istiyor musunuz? eğer istiyorsanız -a- tuşuna basınız.Eğer istemiyorsanız herhangi bir tuş ile çıkış yapınız. ");
+                string Kontrol = Console.ReadLine();
+
+                if (Kontrol != "a")
+                    break;
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
